1ef5aca66decc23f6181fe2959db79711b74e939
wannapornd <wannapornd@hellospec.com>
2022-07-08 12:29:58 +0700

    Delete unnecessary code and adjust width of heading text

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-// // let home = document.getElementById("home");
-// let homeListmenus = document.getElementById("home-listmenus");
-// home.addEventListener("mouseover", () => {
-//   homeListmenus.style.visibility = "visible";
-// });
-// home.addEventListener("mouseleave", () => {
-//   homeListmenus.style.visibility = "hidden";
-// });
-
-function show_dropdown(element) {
-  // console.log(parent.id);
+function showDropdown(element) {
   child = document.getElementById(element.id).childNodes[3];
   child.style.visibility = "visible";
 }
-function hide_dropdown(element) {
+
+function hideDropdown(element) {
   child = document.getElementById(element.id).childNodes[3];
   child.style.visibility = "hidden";
 }
