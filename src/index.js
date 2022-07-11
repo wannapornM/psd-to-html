bdde4d48b19da54655592f22749c14e270ebdf06
wannapornd <wannapornd@hellospec.com>
2022-07-12 01:24:07 +0700

    Create hidden-navigation panel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// .head-main
 function showDropdown(element) {
   child = document.getElementById(element.id).childNodes[3];
   child.style.visibility = "visible";
@@ -6,4 +7,22 @@ function showDropdown(element) {
 function hideDropdown(element) {
   child = document.getElementById(element.id).childNodes[3];
   child.style.visibility = "hidden";
+}
+
+// .nav-hidden
+function toggleItem(item) {
+  const btnExpander = item.childNodes[3];
+  const subListElement = item.childNodes[5];
+
+  item.classList.contains("dropdown")
+    ? item.classList.remove("dropdown")
+    : item.classList.add("dropdown");
+
+  subListElement.className === "sublists"
+    ? subListElement.setAttribute("class", "show-sublists")
+    : subListElement.setAttribute("class", "sublists");
+
+  btnExpander.className === "btnToggle"
+    ? btnExpander.setAttribute("class", "expander")
+    : btnExpander.setAttribute("class", "btnToggle");
 }
