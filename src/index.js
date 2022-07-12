2d4d82a8b0afd14ff83909253d67766c10927af0
wannapornd <wannapornd@hellospec.com>
2022-07-12 15:34:23 +0700

    Fixed main-app when click hamburger icon in moblie viewport

--- src/index.js
+++ src/index.js
@@ -10,19 +10,36 @@ function hideDropdown(element) {
 }
 
 // .nav-hidden
+const mainApp = document.getElementById("main-app");
+const navHiddenPanel = document.getElementById("nav-hidden");
+const hamburgIcon = document.getElementById("hamburger");
+const closeIcon = document.getElementById("hidden-close-icon");
+
+hamburgIcon.addEventListener("click", () => {
+  mainApp.classList.add("fix-screen");
+  navHiddenPanel.style.display = "block";
+});
+
+closeIcon.addEventListener("click", () => {
+  mainApp.classList.remove("fix-screen");
+  navHiddenPanel.style.display = "none";
+});
+
 function toggleItem(item) {
-  const btnExpander = item.childNodes[3];
+  console.log(item);
   const subListElement = item.childNodes[5];
+  const btnExpander = item.childNodes[3];
+  const icon = btnExpander.childNodes[1];
 
-  item.classList.contains("dropdown")
-    ? item.classList.remove("dropdown")
-    : item.classList.add("dropdown");
-
-  subListElement.className === "sublists"
-    ? subListElement.setAttribute("class", "show-sublists")
-    : subListElement.setAttribute("class", "sublists");
-
-  btnExpander.className === "btnToggle"
-    ? btnExpander.setAttribute("class", "expander")
-    : btnExpander.setAttribute("class", "btnToggle");
+  if (item.classList.contains("dropdown")) {
+    item.setAttribute("class", "item");
+    subListElement.setAttribute("class", "sublists");
+    btnExpander.setAttribute("class", "expander");
+    icon.style.transform = "rotate(0deg)";
+  } else {
+    item.setAttribute("class", "change-style-item dropdown");
+    subListElement.setAttribute("class", "show-sublists");
+    btnExpander.setAttribute("class", "btnToggle");
+    icon.style.transform = "rotate(180deg)";
+  }
 }
